e3a692ac86e5353d8bd5432c7f83b12b5d5dedc3
mohd123vaseem <106667385+mohd123vaseem@users.noreply.github.com>
2024-11-02 13:37:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,12 @@ const port = process.env.PORT || 4000;
 app.use(express.json())
 app.use(cors())
 //edit
+app.use(cors({
+  origin: 'https://food-del-1234-frontend.onrender.com',
+  credentials: true
+}));
+
+//edit
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
